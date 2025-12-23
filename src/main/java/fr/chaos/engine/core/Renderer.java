e4685effd3be2ac988466c0a3f2148a9e5e33db4
legeriergeek <legeriergeek@somewebsite.online>
2025-12-23 16:36:08 +0100

    Quick typo fix

--- src/main/java/fr/chaos/engine/core/Renderer.java
+++ src/main/java/fr/chaos/engine/core/Renderer.java
@@ -1,6 +1,5 @@
 package fr.chaos.engine.core;
 
-import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
